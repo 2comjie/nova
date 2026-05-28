f5e47a4a5f043f51fffed160a5903505f80e53d0
zhengyinjie <3217998214@qq.com>
2026-05-29 01:46:48 +0800

    feat(encoding): 添加 XML 到 Map 的解析支持
    - 引入 clbanning/mxj 库用于 XML 解析
    - 使用反射检测目标类型为 Map 或 Interface
    - 当目标为 Map 类型时使用 mxj 解析 XML 数据
    - 保持原有 xml.Unmarshal 功能作为默认处理方式
    - 更新 go.mod 和 go.sum 文件添加新依赖

--- encoding/xml/xml.go
+++ encoding/xml/xml.go
@@ -2,6 +2,9 @@ package xml
 
 import (
 	"encoding/xml"
+	"reflect"
+
+	"github.com/clbanning/mxj"
 )
 
 type XmlCodec struct{}
@@ -11,6 +14,18 @@ func (c *XmlCodec) Marshal(v any) ([]byte, error) {
 }
 
 func (c *XmlCodec) Unmarshal(data []byte, v any) error {
+	rv := reflect.ValueOf(v)
+	if rv.Kind() == reflect.Ptr {
+		rv = rv.Elem()
+	}
+	if rv.Kind() == reflect.Map || rv.Kind() == reflect.Interface {
+		m, err := mxj.NewMapXml(data)
+		if err != nil {
+			return err
+		}
+		rv.Set(reflect.ValueOf(m))
+		return nil
+	}
 	return xml.Unmarshal(data, v)
 }
 
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 require (
 	github.com/BurntSushi/toml v1.6.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/clbanning/mxj v1.8.4 // indirect
 	github.com/fsnotify/fsnotify v1.10.1 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
