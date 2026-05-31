84b0b41bfcec1dced91c2068044cb3e3b6b8ba3c
zhengyinjie <3217998214@qq.com>
2026-05-31 14:00:11 +0800

    feat(pb): 添加 gate 服务的 protobuf 定义文件
    - 定义了 BindRequest 和 BindResponse 消息结构
    - 定义了 UnbindRequest 和 UnbindResponse 消息结构
    - 定义了 GetIpRequest 和 GetIpResponse 消息结构
    - 定义了 IsOnlineRequest 和 IsOnlineResponse 消息结构
    - 定义了 SetAttributeRequest 和 SetAttributeResponse 消息结构
    - 定义了 GetAttributeRequest 和 GetAttributeResponse 消息结构
    - 定义了 DeleteAttributeRequest 和 DeleteAttributeResponse 消息结构
    - 定义了

--- proto_gen.sh
+++ proto_gen.sh
@@ -6,7 +6,10 @@ cd "$(dirname "$0")"
 MODULE="github.com/2comjie/wali"
 PROTOS=$(find . -name '*.proto')
 
+# --go_out        生成 message 结构体 (protoc-gen-go)
+# --go-wali-grpc_out  生成 service 桩代码 (proto-gen-rpc-wali，内含 grpc 生成)
 protoc -I . -I ./proto \
   --go_out=. --go_opt=module="$MODULE" \
-  --go-grpc_out=. --go-grpc_opt=module="$MODULE" \
+  --plugin=protoc-gen-go-wali-grpc="$(go env GOPATH)/bin/proto-gen-rpc-wali" \
+  --go-wali-grpc_out=. --go-wali-grpc_opt=module="$MODULE" \
   $PROTOS
