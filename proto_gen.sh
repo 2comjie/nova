349a0a2acf040ac9768b60692bfb8fd1fbc09140
zhengyinjie <3217998214@qq.com>
2026-07-26 14:00:38 +0800

    feat(proto): 添加网关和节点服务的protobuf定义及代码生成配置
    - 新增 gate.proto 定义Push和Kick RPC接口用于消息推送和踢除功能
    - 新增 node.proto 定义Call和Tell RPC接口用于业务请求处理
    - 创建 protoc-gen-go-grpc-locator 插件的Makefile安装脚本
    - 修改 proto_gen.sh 脚本集成grpc定位器代码生成器替换原有grpc生成器
    - 更新代码生成配置以支持新的服务间通信协议定义

--- proto_gen.sh
+++ proto_gen.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 protoc \
-    --proto_path=proto \
-    --go_out=. \
-    --go_opt=module=github.com/2comjie/wali \
-    --go-grpc_out=. \
-    --go-grpc_opt=module=github.com/2comjie/wali \
-    proto/transport/request/request.proto \
-    proto/transport/gate/gate.proto \
-    proto/transport/node/node.proto
+  --proto_path=proto \
+  --go_out=. \
+  --go_opt=module=github.com/2comjie/wali \
+  --go-grpc-locator_out=. \
+  --go-grpc-locator_opt=module=github.com/2comjie/wali \
+  proto/transport/node/node.proto \
+  proto/transport/gate/gate.proto
